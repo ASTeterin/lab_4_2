19be741b63c026973275e5c653b563d0b95c6f4e
Alex Teterin <at28813905@gmail.com>
2018-04-23 17:44:18 +0300

    модифицирован JS

--- 5_2/js/main.js
+++ 5_2/js/main.js
@@ -3,15 +3,14 @@ window.onload = function() {
     showPopupWindow();
 }
 
-
 function showFilmBlock(){
     var filmButton = document.getElementById('all_films_button');
 
-    function displayFilmBlock() {
+    function displayFilmBlock(event) {
+        event.preventDefault();
         var elem = document.getElementById('hidden_block');
         filmButton.style.display = 'none';
         elem.classList.add('visible_advanced_film');
-        event.preventDefault();
     }
     
     filmButton.addEventListener('click', displayFilmBlock);
@@ -20,17 +19,17 @@ function showFilmBlock(){
 function showPopupWindow(){
     var popupWindow = document.getElementsByClassName('popup')[0];
     var background = document.getElementsByClassName('background')[0];
-    function displayWindow(){
+    
+    function displayWindow(event){
         event.preventDefault();
-        //document.getElementsByTagName('body')[0].classList.add('background');
-        document.getElementsByClassName('background')[0].style.display = 'block';
-        popupWindow.style.display = 'block';
+        background.classList.add('set_visible');
+        popupWindow.classList.add('set_visible');
     }
 
-    function closeWindow(){
+    function closeWindow(event){
         event.preventDefault();
-        background.style.display = 'none';
-        popupWindow.style.display = 'none';
+        background.classList.remove('set_visible');
+        popupWindow.classList.remove('set_visible');
     }
 
     document.getElementById('close_window').addEventListener('click', closeWindow);
@@ -38,39 +37,29 @@ function showPopupWindow(){
     background.addEventListener('click', closeWindow);
 
     function checkForm() {
-        var name = document.getElementById('name');
-        var email = document.getElementById('user_email');
-        var text = document.getElementById('text_area');
-    
-        function setBorder(perem){
-            perem.style.border = '2px solid rgb(255, 0, 0)';
+        var i;
+        var required_fild = document.getElementsByClassName('required_field');
+        
+        event.preventDefault();
+        
+        function setBorder(inputArea){
+            inputArea.classList.add('error');
         }
         
         function removeBorder(){
-            this.style.border = '';
+            this.classList.remove('error');
         }
-    
-        event.preventDefault();
-        if (document.getElementById('name').value == ''){
-            setBorder(name);
+
+        for (i = 0; i < required_fild.length; i++){
+            if (required_fild[i].value == '')
+                setBorder(required_fild[i])
         }
-        if (email.value == ''){
-            setBorder(email);
+        
+        for (i = 0; i < required_fild.length; i++){
+            required_fild[i].addEventListener('focus', removeBorder);
         }
-        if (text_area.value == ''){
-            setBorder(text_area);
-        }
-        name.addEventListener('focus', removeBorder);
-        email.addEventListener('focus', removeBorder);
-        text.addEventListener('focus', removeBorder);
-    
+        
     }
     document.getElementById('send').addEventListener('click', checkForm);
 }
 
-    
-   
-
-
-
-//document.getElementsByName('message')[0].addEventListener('focus', removeBorder);
